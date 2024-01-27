66808d5cc787e8265b90a7806a1fc51d08495bfc
Євген Белей <zenabelej@gmail.com>
2024-01-27 16:12:11 +0200

    Додаю послаблену сутність "fact_tag"

--- app/Models/Fact.php
+++ app/Models/Fact.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 /**
@@ -35,5 +36,10 @@ class Fact extends Model
     public function likes(): MorphMany
     {
         return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,8 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperTag
+ */
 class Tag extends Model
 {
     use HasFactory, HasUuids;
+
+    public function facts(): BelongsToMany
+    {
+        return $this->belongsToMany(Fact::class);
+    }
+
+
 }
