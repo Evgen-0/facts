5bbf856008f227b2ef99a8b0d63e61617dc490b9
Євген Белей <zenabelej@gmail.com>
2024-01-28 11:26:48 +0200

    Додаю casts на поле type з enum MediaType для сутності "media_files"

--- app/Models/MediaFile.php
+++ app/Models/MediaFile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Type\MediaType;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,10 @@ class MediaFile extends Model
     use HasFactory, HasUuids;
 
     protected $with = ['fact'];
+
+    protected $casts = [
+        'type' => MediaType::class,
+    ];
 
     public function fact(): BelongsTo
     {
