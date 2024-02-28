3927e4a567f99634783dd3cff6ed458bbe116abb
Євген Белей <zenabelej@gmail.com>
2024-02-28 23:44:00 +0200

    Додаю стан коли немає нічого в пошуку

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -69,35 +69,56 @@ function submitSearch() {
           </div>
         </div>
       </form>
-      <ul class="mx-2 sm:mx-auto pt-4 max-w-3xl divide-y divide-gray-200 dark:divide-gray-700">
-        {{ facts.data[0] }}
-        <template
-          v-for="fact in facts.data"
-          :key="fact.id"
+      <div v-if="facts.data[0]">
+        <ul
+          class="mx-2 sm:mx-auto pt-4 max-w-3xl divide-y divide-gray-200 dark:divide-gray-700"
         >
-          <Link :href="route('facts.show', fact.slug)">
-            <li class="pb-3 sm:pb-4">
-              <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                <div class="flex-shrink-0">
-                  <img
-                    class="w-8 h-8 rounded-full"
-                    :src="fact.body"
-                    alt="Neil image"
-                  >
-                </div>
-                <div class="flex-1 min-w-0">
-                  <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    {{ fact.heading }}
-                  </p>
-                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                    {{ fact.category.name }}
-                  </p>
-                </div>
+          <template
+            v-for="fact in facts.data"
+            :key="fact.id"
+          >
+            <div>
+              <Link :href="route('facts.show', fact.slug)">
+                <li class="pb-3 sm:pb-4">
+                  <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                    <div class="flex-shrink-0">
+                      <img
+                        class="w-8 h-8 rounded-full"
+                        :src="fact.body"
+                        alt="Neil image"
+                      >
+                    </div>
+                    <div class="flex-1 min-w-0">
+                      <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {{ fact.heading }}
+                      </p>
+                      <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                        {{ fact.category.name }}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </Link>
+            </div>
+          </template>
+        </ul>
+      </div>
+      <div v-else>
+        <ul
+          class="mx-2 sm:mx-auto pt-4 max-w-3xl divide-y divide-gray-200 dark:divide-gray-700"
+        >
+          <li class="pb-3 flex justify-center sm:pb-4">
+            <div class="flex items-center space-x-4 rtl:space-x-reverse">
+              <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                  Немає
+                </p>
+                <p class="text-sm text-gray-500 truncate dark:text-gray-400" />
               </div>
-            </li>
-          </Link>
-        </template>
-      </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </authenticated-layout>
 </template>
