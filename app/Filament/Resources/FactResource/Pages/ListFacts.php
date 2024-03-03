2a1d1ba841cc35b93fea88bd650a266934c1a4dc
Євген Белей <zenabelej@gmail.com>
2024-03-03 18:50:32 +0200

    Задаю, щоб після створення факту створювався запис в таблиці статистика по фактах

--- app/Filament/Resources/FactResource/Pages/ListFacts.php
+++ app/Filament/Resources/FactResource/Pages/ListFacts.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\FactResource\Pages;
 
 use App\Filament\Resources\FactResource;
-use Filament\Actions;
+use App\Models\Fact;
+use App\Models\FactStat;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListFacts extends ListRecords
@@ -13,7 +15,15 @@ class ListFacts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make()
+                ->after(function (CreateAction $action, Fact $record) {
+                    FactStat::create([
+                        'fact_id' => $record->id,
+                        'views' => 0,
+                        'likes' => 0,
+                        'comments' => 0,
+                    ]);
+                }),
         ];
     }
 }
